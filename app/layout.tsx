d3a966facbdeec33409572b049e99410cdf35330
codewithsamir <ajaction05@gmail.com>
2026-05-04 13:56:29 +0545

    update defalutl mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
